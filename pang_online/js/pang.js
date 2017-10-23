1a0a08511e4d676984d1fabab60d4e8e71d9c55e
Eva <evishaemece@gmail.com>
2017-10-23 11:16:21 +0200

    Modificados e introducidos sprites del personaje

--- pang_online/js/pang.js
+++ pang_online/js/pang.js
@@ -7,7 +7,8 @@ $(document).ready(function(){
             game.load.image('sky', 'assets/sky.png');
             game.load.image('ground', 'assets/platform.png');
             game.load.image('star', 'assets/star.png');
-            game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
+            game.load.spritesheet('dude', 'assets/player.png', 32, 32);
+            game.load.spritesheet('dude', 'assets/player_rightmove.png', 32, 32);
             game.load.image('bullet', 'assets/laser_bullet.png');
             }
 
@@ -49,8 +50,8 @@ $(document).ready(function(){
                 player.body.gravity.y = 300;
                 player.body.collideWorldBounds = true;
 
-                player.animations.add('left', [0, 1, 2, 3], 10, true);
-                player.animations.add('right', [5, 6,7, 8], 10, true);
+                player.animations.add('moveleft', [0, 1, 2, 3], 10, true);
+                player.animations.add('moveright', [13, 14, 15, 16], 10, true);
 
                 //MOVIMIENTO
 
@@ -70,14 +71,14 @@ $(document).ready(function(){
                 player.body.velocity.x = 0;
                 if(cursors.left.isDown){
                     player.body.velocity.x = -150;
-                    player.animations.play('left');
+                    player.animations.play('moveleft');
                     if(player.position.x == 0 ){
                         player.position.x = game.world.width;
                     }
                 }
                 else if(cursors.right.isDown){
                     player.body.velocity.x = 150;
-                    player.animations.play('right');
+                    player.animations.play('moveright');
                     if(player.position.x >= game.world.width-40){
                         player.position.x = 0;
                     }   
