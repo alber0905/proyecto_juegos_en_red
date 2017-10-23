120ccdc588f37f2dc15cde5f0f0e967b19b2f66e
alber0905 <alber09055@gmail.com>
2017-10-23 12:40:57 +0200

    El gancho choca contra el techo

--- pang_online/js/pang.js
+++ pang_online/js/pang.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
             var game = new Phaser.Game(600, 600, Phaser.AUTO, 'prueba', { preload: preload, create: create, update: update });
             var platforms;
             var player;
+            var long_bullet_instance;
             var bulletTime = 0;
             function preload(){
             game.load.image('sky', 'assets/sky.png');
@@ -9,7 +10,7 @@ $(document).ready(function(){
             game.load.image('star', 'assets/star.png');
             game.load.spritesheet('dude', 'assets/player.png', 32, 34);
             game.load.image('bullet', 'assets/laser_bullet.png');
-            game.load.image('long_bullet', 'assets/long_bullet.png');
+            game.load.image('long_bullet', 'assets/superlong_bullet.png');
             game.load.image('ball', 'assets/big_red_ball.png');
             }
 
@@ -24,10 +25,13 @@ $(document).ready(function(){
                 platforms = game.add.group();
                 platforms.enableBody = true;
 
-                var ground = platforms.create(0, game.world.height - 64, 'ground', 3);
+                var ground = platforms.create(0, game.world.height - 64, 'ground');
+                var ceil = platforms.create(0, 0, 'ground');
                 ground.scale.setTo(4, 2);
+                ceil.scale.setTo(4,2);
 
                 ground.body.immovable = true;
+                ceil.body.immovable = true;
 
                 //BALAS
 
@@ -63,6 +67,9 @@ $(document).ready(function(){
                 long_bullet_instance = bullets.create(player.x, player.y, 'long_bullet');
                 long_bullet_instance.exists = false;
                 long_bullet_instance.visible = false;
+                long_bullet_instance.collideWorldBounds = true;
+                long_bullet_instance.body.bounce.setTo(0, 0);
+
                 game.world.sendToBack(long_bullet_instance);
                
                 
@@ -80,6 +87,7 @@ $(document).ready(function(){
                 
                 balls = game.add.group();
                 ball = game.add.sprite(400, 200, 'ball');
+                ball.size = 1;
 
 
                 game.physics.enable(ball, Phaser.Physics.ARCADE);
@@ -98,8 +106,6 @@ $(document).ready(function(){
 
                 ball.scale.setTo(scake, scake);
                 ball.body.collideWorldBounds = true;
-                player.body.collideWorldBounds = true;
-                //ball.body.collide('platforms');
                 ball.body.bounce.setTo(1, 1);
                 ball.body.velocity.setTo(100, 100);
                 ball.body.gravity.setTo(0, gravity);
@@ -109,6 +115,7 @@ $(document).ready(function(){
             function update(){
                 var hitPlatform = game.physics.arcade.collide(player, platforms);
                 game.physics.arcade.collide(ball, platforms);
+                game.physics.arcade.collide(bullets, platforms);
 
 
                 player.body.velocity.x = 0;
@@ -132,6 +139,10 @@ $(document).ready(function(){
                     player.frame = 4;
                 } 
 
+                if(long_bullet_instance.position.y > game.world.height){
+                    stopLongBullet(long_bullet_instance);
+                }
+
                 
             }
 
@@ -154,6 +165,11 @@ $(document).ready(function(){
             function fireLongBullet(){
                 long_bullet_instance.reset(player.x, player.y);
                 long_bullet_instance.body.velocity.y = -200;
+            }
+
+            function stopLongBullet(long_bullet){
+               long_bullet.kill();
+               console.log("Fuera");
             }
 
         });
