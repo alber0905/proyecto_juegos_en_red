4304cfaf07a3c4131ab8e9d75a41921f0eebc0f2
jetese <jetese2@gmail.com>
2017-11-15 12:16:30 +0100

    Implementado get

--- Servidor/src/main/java/es/urjc/code/rest/MainController.java
+++ Servidor/src/main/java/es/urjc/code/rest/MainController.java
@@ -18,13 +18,14 @@ import java.io.Writer;
 import java.lang.ProcessBuilder.Redirect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class MainController {
 	
-	@PostMapping("/anuncios")
+	@PostMapping("/UpdatePuntuaciones")
         @ResponseStatus(HttpStatus.CREATED)
-	public List<Puntuacion> anuncios(@RequestBody Puntuacion anuncio) throws IOException {
+	public List<Puntuacion> anuncios(@RequestBody Puntuacion newPunt) throws IOException {
             BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/static/json_files/Puntuacion.json"));
 
             Gson gson = new GsonBuilder().create();
@@ -36,10 +37,25 @@ public class MainController {
             reader.close();
 		return puntuaciones;
 	}
+        
+        @GetMapping("/GetPuntuaciones")
+        @ResponseStatus(HttpStatus.CREATED)
+	public List<Puntuacion> getPuntuaciones() throws IOException {
+            BufferedReader reader = new BufferedReader(new FileReader("./src/main/resources/static/json_files/Puntuacion.json"));
+
+            Gson gson = new GsonBuilder().create();
+//            gson.toJson("Hello", writer);
+//            gson.toJson(345, writer);
+            java.lang.reflect.Type listType = new TypeToken<List<Puntuacion>>(){}.getType();
+            List<Puntuacion> puntuaciones = gson.fromJson(reader, listType);
+
+            reader.close();
+		return puntuaciones;
+	}
+        
         @PostMapping("/prueba")
         @ResponseStatus(HttpStatus.CREATED)
 	public String prueba() {
 		return "HOla";
 	}
-
 }
