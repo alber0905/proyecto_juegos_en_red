bb78d370d85a615bae7290da3ef238ab57bcd7b3
Alberto Blanco <alber09055@gmail.com>
2017-11-29 12:22:07 +0100

    Incluidos archivos java para websockets (con la plantilla del chat)

--- Servidor/src/main/java/es/urjc/code/rest/RestApplication.java
+++ Servidor/src/main/java/es/urjc/code/rest/RestApplication.java
@@ -3,12 +3,28 @@ package es.urjc.code.rest;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+
 
 @SpringBootApplication
-public class RestApplication {
+public class RestApplication implements WebSocketConfigurer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RestApplication.class, args);
 
+	}
+        
+        @Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+		registry.addHandler(createChatHandler(), "/chat")
+			.setAllowedOrigins("*");
+	}
+	
+	@Bean
+	public PangSocketHandler createChatHandler() {
+		return new PangSocketHandler();
 	}
 }
